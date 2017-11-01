31c7dc84bc5fd1f90cfca9a89316fe40d34ad703
iriesatoshi <hirodai10073104@yahoo.co.jp>
2017-11-01 21:10:25 +0900

    ユーザー情報編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
+
+  resources :users, only: [:edit, :update]
+  # get '/users/:id/edit' => 'users#edit'
+  # put '/users/:id/update' => 'users#update'
+
 end
