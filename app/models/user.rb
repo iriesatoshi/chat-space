13e033384665f1a488a896ba0a10577cccc72a32
iriesatoshi <hirodai10073104@yahoo.co.jp>
2017-11-02 01:26:27 +0900

    Flashメッセージ機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :name, presence: true
+  validates :email, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
 
