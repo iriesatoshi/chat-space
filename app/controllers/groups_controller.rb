771fd64e7dbb9a3a4da8e1c5d0f18deec47efb62
iriesatoshi <hirodai10073104@yahoo.co.jp>
2017-11-03 20:46:37 +0900

    グループ編集機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,11 +15,17 @@ class GroupsController < ApplicationController
   end
 
   def edit
-
+    @group = Group.find(params[:id])
   end
 
   def update
-
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+    redirect_to root_path
+    flash[:notice] = "グループを更新しました"
+    else
+      render "edit"
+    end
   end
 
     private
