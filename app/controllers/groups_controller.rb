b87f8e45a42b26eea6e5af03dcd43702ecc6e796
iriesatoshi <hirodai10073104@yahoo.co.jp>
2017-11-03 18:47:52 +0900

    グループ作成機能修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,7 +7,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-    redirect_to controller: 'messages', action: 'index'
+    redirect_to root_path
     flash[:notice] = "グループを作成しました"
     else
       render "new"
