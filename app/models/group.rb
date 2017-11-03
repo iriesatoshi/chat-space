1e8dbc6f6a682ad14e4d0bd16ce83af177ce8f39
iriesatoshi <hirodai10073104@yahoo.co.jp>
2017-11-03 18:30:41 +0900

    グループ作成機能の実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
+  validates :group_name, presence: true
   has_many :users, through: :members
   has_many :members
   has_many :messages
--- db/migrate/20171103074146_create_members.rb
+++ db/migrate/20171103074146_create_members.rb
@@ -2,7 +2,7 @@ class CreateMembers < ActiveRecord::Migration[5.0]
   def change
     create_table :members do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :group, null: false, foreign_key: true
+      t.references :group, foreign_key: true
       t.timestamps
     end
   end
