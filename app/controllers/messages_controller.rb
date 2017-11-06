871e57db48f9c4b6e23b561712f801b9c708fc44
iriesatoshi <hirodai10073104@yahoo.co.jp>
2017-11-06 19:15:24 +0900

    メッセージの保存とバリデーション及びフラッシュ

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,26 @@
 class MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)
+      if @message.save
+        flash[:notice] = "メッセージを送信しました"
+        redirect_to group_messages_path
+      else
+        @group = Group.find(params[:group_id])
+        flash.now[:alert] = "メッセージを入力してください"
+        render "index"
+      end
+  end
+
+  private
+
+   def message_params
+     params.require(:message).permit(:body,:image).merge(group_id: params[:group_id]).merge(user_id: current_user.id)
+   end
+
+
 end
